a09c07fa7abb27b72b2d3d80cfbec88153563176
7com <bl4ckt3ch@gmail.com>
2017-12-19 17:52:40 -0300

    Creación clase Adquisición
    19-12-17

--- src/Serial/Adquisicion.java
+++ src/Serial/Adquisicion.java
@@ -6,7 +6,6 @@
 package Serial;
 
 import com.fazecast.jSerialComm.SerialPort;
-import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -17,19 +16,20 @@ import javax.swing.JOptionPane;
  *
  * @author fivc
  */
-public class Terminal extends javax.swing.JFrame {
+public class Adquisicion extends javax.swing.JFrame {
 
-    public SerialPort scorbot;
+    public SerialPort scorbot,arduino;
     private String captura="";
     private char ser;
-    private String[] enco,encoAnterior;     
+    private String[] enco,encoAnterior;
+    private boolean capturar=false;
     
     Thread lectura = new Thread() {
         InputStream in;
         public void run() {
            scorbot.openPort();
            in = scorbot.getInputStream();
-           while (true){
+           while (capturar){
                
                try 
                {
@@ -53,18 +53,18 @@ public class Terminal extends javax.swing.JFrame {
                     
                     else
                     {
+                        if(encoAnterior.length != 0){
+                            for(int i=0; i<6; i++)
+                            {
+                                //System.out.print(enco[i]+" ");
+                            }
+                            // System.out.println();
+                        }
+                        
                         if(!captura.isEmpty())
                         {
                             String temp = captura.replaceAll("[0-9-]","");
-                            jTextArea1.setText(jTextArea1.getText()+temp+"\n");
-                            jTextArea1.setCaretPosition(jTextArea1.getText().length());
-                            if(encoAnterior.length != 0){
-                                for(int i=0; i<6; i++)
-                                {
-                                    //System.out.print(enco[i]+" ");
-                                }
-                               // System.out.println();
-                            }
+                            
                         }
                     }
                     captura="";
@@ -77,10 +77,11 @@ public class Terminal extends javax.swing.JFrame {
            }
         }  
     };
-
-    /**
-     * Creates new form Terminal
-     */
+    
+    public Adquisicion() {
+        initComponents();
+        
+    }   
     
     private boolean esEnco(String[] enco)
     {
@@ -129,9 +130,6 @@ public class Terminal extends javax.swing.JFrame {
         }
     }
     
-    public Terminal() {
-        initComponents();
-    }
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -142,31 +140,31 @@ public class Terminal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
-        jTextField1 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
+        jToggleButton1 = new javax.swing.JToggleButton();
+
+        jLabel2.setText("jLabel2");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Servicio de Adquisición de Parámetros Scorbot");
         setBackground(new java.awt.Color(255, 255, 255));
 
-        jTextArea1.setEditable(false);
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
-        jTextArea1.setDragEnabled(true);
-        jTextArea1.setDropMode(javax.swing.DropMode.INSERT);
         jScrollPane1.setViewportView(jTextArea1);
 
-        jTextField1.addKeyListener(new java.awt.event.KeyAdapter() {
-            public void keyReleased(java.awt.event.KeyEvent evt) {
-                jTextField1KeyReleased(evt);
-            }
-        });
+        jLabel1.setText("Consola de Mensajes:");
 
-        jButton1.setText("CTRL+C");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.setText("Iniciar Terminal");
+
+        jToggleButton1.setText("Iniciar Captura");
+        jToggleButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                jToggleButton1ActionPerformed(evt);
             }
         });
 
@@ -177,54 +175,64 @@ public class Terminal extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jTextField1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabel1)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jToggleButton1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 132, Short.MAX_VALUE)
                         .addComponent(jButton1)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(9, 9, 9)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 171, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jToggleButton1)
                     .addComponent(jButton1))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField1KeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextField1KeyReleased
-        if(evt.getKeyCode() == KeyEvent.VK_ENTER) {
+    private void jToggleButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jToggleButton1ActionPerformed
+        if (!capturar)
+        {
+            jToggleButton1.setSelected(true);
             OutputStream out = scorbot.getOutputStream();
             PrintStream printStream = new PrintStream(out);
-            String enviar = jTextField1.getText()+" \r";
+            String enviar = "show enco \r";
             printStream.print(enviar);
             printStream.close();
-            jTextField1.setText("");
+            capturar=true;
+            this.lectura.start();
         }
-    }//GEN-LAST:event_jTextField1KeyReleased
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        OutputStream out = scorbot.getOutputStream();
-        PrintStream printStream = new PrintStream(out);
-        char enviar = (char) 3;
-        printStream.print(enviar);
-        printStream.close();
-    }//GEN-LAST:event_jButton1ActionPerformed
-
-   
+        else
+        {
+            capturar=false;
+            jToggleButton1.setSelected(false);
+            OutputStream out = scorbot.getOutputStream();
+            PrintStream printStream = new PrintStream(out);
+            char enviar = (char) 3;
+            printStream.print(enviar);
+            printStream.close();   
+        }
+    }//GEN-LAST:event_jToggleButton1ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JToggleButton jToggleButton1;
     // End of variables declaration//GEN-END:variables
 }
--- src/Serial/MenuConfiguracion.java
+++ src/Serial/MenuConfiguracion.java
@@ -29,7 +29,10 @@ public class MenuConfiguracion extends javax.swing.JFrame {
             System.exit(1);
         }
         for(int i=0; i<comPort.length ; i++)
+        {
             jComboBox1.addItem(new ComboItem(comPort[i].getDescriptivePortName(),comPort[i]));
+            jComboBox2.addItem(new ComboItem(comPort[i].getDescriptivePortName(),comPort[i]));
+        }   
     }
     
     /**
@@ -43,8 +46,21 @@ public class MenuConfiguracion extends javax.swing.JFrame {
 
         jComboBox1 = new javax.swing.JComboBox<>();
         jButton1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jComboBox2 = new javax.swing.JComboBox<>();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Servicio de Adquisición de Parámetros Scorbot");
+        setBackground(new java.awt.Color(255, 255, 255));
+
+        jComboBox1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jComboBox1ActionPerformed(evt);
+            }
+        });
 
         jButton1.setText("Iniciar");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -53,45 +69,92 @@ public class MenuConfiguracion extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setText("Puerto Controlador:");
+
+        jLabel2.setText("Puerto Arduino Mega:");
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Scorbot.png"))); // NOI18N
+
+        jLabel4.setText("Nombre");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(135, Short.MAX_VALUE)
-                .addComponent(jButton1)
-                .addGap(124, 124, 124))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jComboBox2, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 201, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(132, 132, 132))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(117, 117, 117)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 175, Short.MAX_VALUE)
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(35, 35, 35)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jComboBox2, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 63, Short.MAX_VALUE)
                 .addComponent(jButton1)
-                .addGap(45, 45, 45))
+                .addGap(32, 32, 32))
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         ComboItem i = (ComboItem)jComboBox1.getSelectedItem();
-        Terminal t = new Terminal();
-        t.scorbot = i.getPuerto();
-        this.setVisible(false);
-        t.setVisible(true);
-        t.lectura.start();
+        ComboItem j = (ComboItem)jComboBox2.getSelectedItem();
+        
+        //if (i.getPuerto() != j.getPuerto())
+        if(true)
+        {
+            Adquisicion a = new Adquisicion();
+            a.scorbot = i.getPuerto();
+            //a.arduino = j.getPuerto();
+            this.setVisible(false);
+            a.setVisible(true);
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jComboBox1ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JComboBox<ComboItem> jComboBox1;
+    private javax.swing.JComboBox<ComboItem> jComboBox2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     // End of variables declaration//GEN-END:variables
 }
 
