c67181962621f45928e4aff90fb16ba1cb08ec28
Felipe Valenzuela Cornejo <bl4ckt3ch@gmail.com>
2018-02-26 17:05:45 -0300

    lockfile 26-02-2018

--- src/main.java
+++ src/main.java
@@ -10,14 +10,27 @@
    *  Memoria para optar al título de Ingeniero Civil en Informática  *
    ********************************************************************/
 import Serial.MenuConfiguracion;
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JOptionPane;
 
 public class main {
     
     public static void main(String[] args){
-        //Se crea menú de configuración y se cargan puertos Seriales
-        MenuConfiguracion m = new MenuConfiguracion();
-        m.cargarPuertos();
-        m.setVisible(true);
+        File lock = new File("lock");
+        try {
+            if (lock.createNewFile()){
+                lock.deleteOnExit();
+                //Se crea menú de configuración y se cargan puertos Seriales
+                MenuConfiguracion m = new MenuConfiguracion();
+                m.cargarPuertos();
+                m.setVisible(true);
+            } else {
+                System.exit(1);
+            }
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null,ex.getMessage(),"Error",JOptionPane.ERROR_MESSAGE);
+        }
   }
     
 }
